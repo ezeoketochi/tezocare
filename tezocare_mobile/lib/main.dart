14bb11a206a062406316fb06f9a2ef4a5cc3abc7
tezo <tochioluebube@gmail.com>
2026-05-10 19:42:40 +0100

    implemented fireabase config and fixed app load, app loading successfully now to the spalshscreen, the getIt DI is also good

--- tezocare_mobile/lib/main.dart
+++ tezocare_mobile/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tezocare_mobile/firebase_options.dart';
 import 'config/routes/app_router.dart';
 import 'config/themes/app_theme.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    name: 'TezoCare',
-  );
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  debugPrint('Firebase initialized successfully, DI is next');
+
   await di.init();
   runApp(const TezoCareApp());
 }
@@ -19,6 +22,7 @@ class TezoCareApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appRouter = di.sl<AppRouter>();
+    debugPrint('AppRouter initialized: $appRouter');
     return MaterialApp.router(
       title: 'TezoCare',
       theme: AppTheme.lightTheme,
--- tezocare_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tezocare_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import connectivity_plus
 import firebase_core
 import firebase_messaging
-import flutter_local_notifications
 import flutter_secure_storage_macos
 import printing
 
@@ -16,7 +15,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
 }
