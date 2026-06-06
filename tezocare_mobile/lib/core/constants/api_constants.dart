15843ab4009517ea156ec276f181994416b54554
tezo <tochioluebube@gmail.com>
2026-06-06 17:57:38 +0100

    fix interceptor issue

--- tezocare_mobile/lib/core/constants/api_constants.dart
+++ tezocare_mobile/lib/core/constants/api_constants.dart
@@ -1,8 +1,8 @@
 class ApiConstants {
   ApiConstants._();
 
-  //static const String baseUrl = 'http://192.168.0.119:8000';
-  static const String baseUrl = 'http://192.168.8.102:8000';
+  static const String baseUrl = 'http://192.168.8.105:8000';
+  // static const String baseUrl = 'http://192.168.8.102:8000';
 
   static const String login = '/api/v1/auth/login';
   static const String register = '/api/v1/auth/register';
